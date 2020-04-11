55e4ff0cdf2db5b55925e665243cc98b34989268
Sarah Hancock <sarah.hancock@columbia.edu>
2020-04-10 20:26:59 -0700

    Add html

--- webserver/static/index.js
+++ webserver/static/index.js
@@ -1,25 +1,9 @@
-function addResult(city) {
-    var city_name = city
-
-    var card_container = $("<div class = 'card_container'>")
-
-    var card = $("<div class='card'>")
-    card.attr('id', city_name)
-
-
-    var body = $("<div class='card-body'>")
-    var title = $("<h5 class='card-title'>")
-    title.append(city_name)
-
-    body.append(title)
-    card.append(body)
-    card_container.append(card)
-    $("#results").append(card_container)
-}
 
 $(document).ready(function(){
-    for (city in cities){
-	alert(city)
-        addResult(city)
-    }
+    alert("hi!")
+    $(".card").click(function(){
+        alert(this.id)
+        route = "/view/" + this.id
+        window.location.href = route
+    })
 })
